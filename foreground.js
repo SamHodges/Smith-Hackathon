f11d21b0568b43951befa535c0874b631d742195
Sam <samhodges121@gmail.com>
2021-03-26 19:32:37 -0700

    made paste a separate function for button +auto

--- foreground.js
+++ foreground.js
@@ -11,32 +11,33 @@ var titles;
 document.querySelector('body').appendChild(paste);
 document.querySelector('body').appendChild(copy);
 
-paste.addEventListener('click', () => {
-	var i;
-	var insert = ""; 
-	var x = document.getElementsByClassName("goog-inline-block");
-	for (i = 0; i < titles.length; i++) {
-  		insert = title[i] + "<br>";
-  		//sort according to header types
-
-	}
-});
+paste.addEventListener('click', pasting);
 
 copy.addEventListener('click', () => {
 	chrome.runtime.sendMessage({message: 'yo check the storage'});
 	console.log("I SENT THE MESSAGE");
 	//open new window
 	var newWindow = window.open('https://docs.google.com/document/create');
+	
+	//INSERT WAITING HERE
 
-	//document.onload = updateText(){ 
-		console.log("yipeeeeee")
-		new_html = "testing testing is this working";
-		var x = document.getElementsByClassName("goog-inline-block");
-		x[0].innerHTML = new_html;
-		console.log(x);
-	//};
+	//pasting();
+
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse)=> {
 	console.log(request.message)
 });
+
+
+function pasting(){
+	var i;
+	var insert = ""; 
+	var x = document.getElementsByClassName("goog-inline-block");
+	for (i = 0; i < titles.length; i++) {
+  		insert = title[i] + "<br>";
+  		//sort according to header types
+	}
+	x[0].innerHTML = insert;
+	console.log(insert);
+}
