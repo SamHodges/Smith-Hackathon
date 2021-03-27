56a8cccf97ac91ba7308637495ce1bdc56dd52b8
Sam <samhodges121@gmail.com>
2021-03-26 21:52:14 -0700

    inserts random stuff in right place, but doesn't save

--- foreground.js
+++ foreground.js
@@ -33,14 +33,31 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse)=> {
 function pasting(){
 	var i;
 	var insert = ""; 
-	var x = document.getElementsByClassName("goog-inline-block kix-lineview-text-block");
+	//var x = document.getElementsByClassName("goog-inline-block kix-lineview-text-block");
+	var page = document.getElementsByClassName("kix-page-column")[0];
+	var first = page.getElementsByTagName("div")[0];
+	var second = first.getElementsByTagName("div")[0];
+	var third = second.getElementsByTagName("div")[0];
 
+	
 
 	titles = ["header 1", "header 2", "header 3"];
 	for (i = 0; i < titles.length; i++) {
-  		insert = insert + titles[i] + "<br>";
+  		insert = insert
+  		+ '<div class="kix-paragraphrenderer" id="" style="padding-top: 0px; padding-bottom: 0px;">'
+  		+ '<div class="kix-lineview kix-lineview-z-index" style="height: 19px; direction: ltr; text-align: left;">'
+  		+ '<div class="kix-lineview-content" style="margin-left: 0px; padding-top: 0px; top: -1px;">'
+  		+'<span style="display: inline-block; height: 10000px">'
+  		+'</span>'
+  		+'<span style="display: inline-block; position:relative; top: -9986.242838541666px;">'
+  		+'<span class="goog-inline-block kix-lineview-text-block" style="width: 87px; padding-left: 0px;">'
+  		+'<span class="kix-wordhtmlgenerator-word-node" style="font-size:14.666666666666666px;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;">' 
+  		+ titles[i] 
+  		+ '<span class="goog-inline-block" style="width:7.877604007720947px;height:16.865234375px">'
+  		+' &nbsp;</span>‌</span></span></span></div></div></div>';
   		//sort according to header types
 	}
-	x[0].innerHTML = insert + '<span class="goog-inline-block" style="width:7.877604007720947px;height:16.865234375px">&nbsp;</span>"';
-	console.log(insert);
+	third.innerHTML = insert 
+	//<div class="kix-paragraphrenderer" id="" style="padding-top: 0px; padding-bottom: 0px;"><div class="kix-lineview kix-lineview-z-index" style="height: 19px; direction: ltr; text-align: left;"><div class="kix-lineview-content" style="margin-left: 0px; padding-top: 0px; top: -1px;"><span style="display: inline-block; height: 10000px"></span><span style="display: inline-block; position:relative; top: -9986.242838541666px;"><span class="goog-inline-block kix-lineview-text-block" style="width: 87px; padding-left: 0px;"><span class="kix-wordhtmlgenerator-word-node" style="font-size:14.666666666666666px;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;">Hu‌ ‌frnio3jifj3r‌<span class="goog-inline-block" style="width:7.877604007720947px;height:16.865234375px">&nbsp;</span>‌</span></span></span></div></div></div>
 }
+
